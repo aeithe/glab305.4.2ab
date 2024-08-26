690cbe696aa11fd3a940efd1826d2c7ad7bf2264
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-08-26 09:20:02 -0400

    first commit

---
+++ src/main/java/com/test/hib/controller/App.java
@@ -0,0 +1,14 @@
+package com.test.hib.controller;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        findUser_Hql u = new findUser_Hql();
+        // u.findUser();
+//  u.findUserSelect();
+// u.getRecordbyId();
+        u.getmaxSalary();
+    }
+
+}
